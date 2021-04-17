d0c9e98a777f4d2461322bd1284e590e046d55e7
ElipticNight <secondary@aidan-byrne.org.uk>
2021-04-17 01:17:23 +0100

    responsive grid for projects

--- src/views/sections/Projects.vue
+++ src/views/sections/Projects.vue
@@ -29,10 +29,13 @@ export default {
 
 <style lang="scss" scoped>
 .projects-container {
-  min-height: 400px;
+  margin: 50px 0;
   display: grid;
   grid-template-columns: 1fr 1fr;
   column-gap: 50px;
   row-gap: 50px;
+  @media screen and (max-width: $xl) {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
