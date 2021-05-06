4ea786aba6cb5b233feb825540d2aeb25251307d
ElipticNight <secondary@aidan-byrne.org.uk>
2021-05-06 14:31:05 +0100

    base to new project components

---
+++ client/src/components/home/Project.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import ProjectDefault from "@/components/home/ProjectDefault.vue";
+import ProjectExpanded from "@/components/home/ProjectExpanded";
+
+export default {
+  name: "Project",
+  components: {
+    ProjectDefault,
+    ProjectExpanded,
+  },
+  props: {
+    projectID: {
+      type: Number,
+      required: true
+    }
+  },
+  mounted() {
+    //fetch project info
+  },
+  methods: {
+    //this will be used until i store images on the server
+    getImgUrl(file) {
+      let image = require("@/assets/images/" + file);
+      return image;
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/src/components/home/ProjectDefault.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectDefault",
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    summary: {
+      type: String,
+      default: null,
+    },
+    sourceLink: {
+      type: String,
+      default: null,
+    },
+    liveLink: {
+      type: String,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
