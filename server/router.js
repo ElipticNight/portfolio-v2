8cc8d8b6d71f48c9397106f13be7c3a791243ae0
ElipticNight <secondary@aidan-byrne.org.uk>
2021-05-08 21:08:02 +0100

    moved cors to nginx

--- server/router.js
+++ server/router.js
@@ -1,21 +1,18 @@
 const Projects = require('./controllers/projects');
 
 const cors = require('cors');
-const corsOptions = {
-	origin: ["http://localhost:8080", "https://aidanbyrne.tech"],
-}
 
 var express = require('express'),
 	router = express.Router();
 	router.use(express.json());
 
-	router.get('/project/:id', cors(corsOptions), (req, res) => {
+	router.get('/project/:id', (req, res) => {
 		(async() => {
 			res.send(await Projects.get(req.params.id));
 		})();
 	})
 
-	router.get('/project/:id/images/:imageNo', cors(corsOptions), (req, res) => {
+	router.get('/project/:id/images/:imageNo', (req, res) => {
 		(async() => {
 			let image = await Projects.getImage(req.params.id, req.params.imageNo);
 			res.sendFile(__dirname + `/images/${image.filename}`);
