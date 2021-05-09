d8d2202a317c504007f7f54da8f1a0ddcbbe02cf
ElipticNight <secondary@aidan-byrne.org.uk>
2021-05-09 10:12:14 +0100

    added new route

---
+++ client/src/views/Personal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header />
+    <div class="temp"><h1>Coming Soon !</h1></div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "Personal",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.temp {
+  @extend %center-content-virtical;
+  height: calc(100vh - 65px);
+  width: 100%;
+}
+</style>
--- client/src/views/sections/AboutMe.vue
+++ client/src/views/sections/AboutMe.vue
@@ -14,7 +14,7 @@
           link="https://github.com/ElipticNight"
           text="View GitHub"
         />
-        <LinkButton target="AboutMe" text="Find out more" />
+        <LinkButton target="Personal" text="Find out more" />
       </template>
     </SectionTemplate>
   </div>
