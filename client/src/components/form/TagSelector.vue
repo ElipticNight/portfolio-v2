e33597477aa5426523b0cefce5112c1b9274b705
ElipticNight <secondary@aidan-byrne.org.uk>
2021-05-13 22:39:00 +0100

    added a tag selector component

---
+++ client/src/components/form/TagSelector.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tag-container">
+    <div
+      v-for="(tag, index) in tags"
+      :key="'tag-' + index"
+      class="tag"
+      @click="toggleTag(index)"
+      v-bind:class="{ selected: tags[index].selected === true }"
+    >
+      {{ tag.name }}
+    </div>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+
+export default {
+  name: "Tagselector",
+  props: {
+    value: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      tags: this.value,
+    };
+  },
+  methods: {
+    toggleTag(index) {
+      if (this.tags[index].selected !== undefined) {
+        Vue.delete(this.tags[index], "selected");
+      } else {
+        Vue.set(this.tags[index], "selected", true);
+      }
+      this.$emit("input", this.tags);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.tag-container {
+  @extend %center-content-wrap;
+  .tag {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 15px;
+    border-radius: 5px;
+    margin-right: 10px;
+    background-color: $xdlavender-grey;
+    font-size: 14px;
+    user-select: none;
+    &.selected {
+      background-color: $blue;
+      color: $llavender-grey;
+    }
+  }
+}
+</style>
--- client/src/views/Projects.vue
+++ client/src/views/Projects.vue
@@ -1,8 +1,11 @@
 <template>
   <PageLayout>
     <template v-slot:page-content>
-      <!-- <div class="tags"></div> -->
-      <Divider v-if="false" />
+      <div class="tags">
+        <TagSelector v-model="tags" />
+        {{tags}}
+      </div>
+      <Divider />
       <div v-if="!loading" class="projects">
         <div
           v-for="project in projects"
@@ -18,6 +21,7 @@
 
 <script>
 import PageLayout from "@/views/PageLayout.vue";
+import TagSelector from "@/components/form/TagSelector.vue";
 import Divider from "@/components/misc/PageDivider.vue";
 import Project from "@/components/projects/Project.vue";
 import axios from "axios";
@@ -26,6 +30,7 @@ export default {
   name: "Projects",
   components: {
     PageLayout,
+    TagSelector,
     Divider,
     Project,
   },
@@ -33,6 +38,14 @@ export default {
     return {
       loading: true,
       projects: [],
+      tags: [
+        {
+          name: "one",
+        },
+        {
+          name: "two",
+        },
+      ]
     };
   },
   created() {
@@ -50,11 +63,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// .projects-container {
-//   @extend %center-content-vertical;
-//   height: 100%;
-//   width: 100%;
-//   margin-bottom: 100px;
 ::v-deep .divider {
   width: 15vw;
 }
@@ -91,5 +99,4 @@ export default {
     grid-template-columns: repeat(4, 1fr);
   }
 }
-// }
 </style>
